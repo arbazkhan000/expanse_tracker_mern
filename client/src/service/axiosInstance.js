c118046a0714a109d92f1b9f463282588f4ad3c9
Arbazx01 <mdarbaz9675@gmail.com>
2025-06-01 22:40:41 +0530

    rename baseUrl

--- client/src/service/axiosInstance.js
+++ client/src/service/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:8000/api",
+    baseURL: "https://expanse-tracker-mern-backend.vercel.app/api",
     headers: {
         "Content-Type": "application/json",
         Accept: "application/json",
